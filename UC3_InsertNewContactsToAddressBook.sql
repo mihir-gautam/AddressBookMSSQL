0f131f9f7b0e22f3991724ee0355b621f6a49990
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-11-02 01:02:44 +0530

    [Mihir] Add. Query to insert new contact to address book

---
+++ UC3_InsertNewContactsToAddressBook.sql
@@ -0,0 +1,6 @@
+--UC3 ability to insert new contact
+insert into address_book_table(first_name,last_name,address,city,state,zip,phone,email)
+values ('Anil','Kumar','house no 20','New Delhi','Delhi',110021,9988000000,'xyz@example.com')
+values ('Babu','Lal','house no 21','Amritsar','Punjab',130421,8899000000,'abc@example.com')
+
+select * from address_book_table;
