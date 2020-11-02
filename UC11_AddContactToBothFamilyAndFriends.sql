3450e036300a67dac94cf7a148fdaacc16f55a21
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-11-02 23:25:45 +0530

    [Mihir] Add. Query to add contact to both family and friends added

---
+++ UC11_AddContactToBothFamilyAndFriends.sql
@@ -0,0 +1,9 @@
+--UC11 Ability to add person to both friend and family
+update address_book_table
+set book_name = 'Family Book', book_type = 'Family'
+where last_name = 'Kumar'
+
+insert into address_book_table(first_name,last_name, address,city,state,zip,phone,email,book_name,book_type)
+values ('Anil','Kumar','house no 20','New Delhi','Delhi',110021,9988000000,'xyz@example.com','Friends Book','Friends')
+
+select * from address_book_table
